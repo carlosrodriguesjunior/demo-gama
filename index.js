0c4e1aa12d7bb19634670b61dfe306f3eca2b203
Carlos de Souza Rodrigues Junior <carlao@Carloss-MacBook-Pro.local>
2017-04-07 23:09:20 -0300

    get with database

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var MongoClient = require('mongodb').MongoClient
+var db = null
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+
+    db.collection('teste').find().toArray(function (err, result) {
+        if (err) throw err
+
+        res.json(result)
+        console.log(result)
+    })
+
 })
 
 app.post('/', function (req, res) {
@@ -24,16 +32,13 @@ app.delete('/user', function (req, res) {
 })
 
 
-MongoClient.connect('mongodb://demoUser:gama@ds023490.mlab.com:23490/demo', function (err, db) {
-  if (err) throw err
-
-  db.collection('teste').find().toArray(function (err, result) {
+MongoClient.connect('mongodb://demoUser:gama@ds023490.mlab.com:23490/demo', function (err, _db) {
     if (err) throw err
 
-    console.log(result)
-  })
-})
+    db = _db
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    })
+
 })
