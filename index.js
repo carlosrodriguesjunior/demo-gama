2b826de72218ec2983cd2d5a42bbc9f4e6e317a0
Carlos de Souza Rodrigues Junior <carlao@Carloss-MacBook-Pro.local>
2017-04-07 22:24:25 -0300

    body parser

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.post('/', function (req, res) {
-  res.send('Got a POST request')
+    console.log(req.body);
+    res.json(req.body.teste + 1);
 })
 
 app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
+    console.log(res.body)
+    res.send('Got a PUT request at /user')
 })
 
 app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
+    res.send('Got a DELETE request at /user')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
